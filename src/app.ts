170eef35ad2713e67d65f9da93f860518fafcb95
Breno Silva <brenoqn2@gmail.com>
2022-07-14 00:26:26 -0300

    added main router to server

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import errorHandler from "./middlewares/errorHandler.js";
+import router from "./routers/index.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(router);
 app.use(errorHandler);
 
 app.listen(process.env.PORT || 4000, () => {
