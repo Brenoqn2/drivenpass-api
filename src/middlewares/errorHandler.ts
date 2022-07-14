d747e5319c77ccfad5a4618ca04b59fa91eb5f0b
Breno Silva <brenoqn2@gmail.com>
2022-07-14 00:35:05 -0300

    added validateSchema middleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,6 +2,10 @@ import { ErrorRequestHandler } from "express";
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.error(err);
+
+  if (err.type === "error_schema") {
+    res.status(422).send(err.message);
+  }
   res.status(500).send("Something broke!");
 
   next(err);
