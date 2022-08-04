6f058d672a580004a4f2337d346b688a401a65c0
Guga <contato.gustavobarbosa@outlook.com>
2022-08-04 14:52:58 -0300

    Preparando revisão de código

--- src/schemas/notesSchemas.ts
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const createNoteSchema = joi.object({
-  title: joi.string().max(50).required(),
-  content: joi.string().max(1000).required(),
-});
-
-const notesSchemas = { createNoteSchema };
-export default notesSchemas;
